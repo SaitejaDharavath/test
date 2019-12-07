4e7f0b70477ecd81f219978156c62bbd980d4410
Lab-1 <Lab-1@DESKTOP-UHM8TAB>
2019-12-07 13:28:57 +0530

    this is my second project

---
+++ OracleJavaProAssign2/src/Oracle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Sai
+{
+	Scanner s=new Scanner(System.in);
+	int a=s.nextInt();
+	int b=s.nextInt();
+	void add(int a,int  b)
+	{
+		int c;
+		c=a+b;
+		System.out.println(c);
+		
+	}
+}
+public class Oracle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sai u=new Sai();
+		u.add(5,10);
+		
+
+	}
+
+}
